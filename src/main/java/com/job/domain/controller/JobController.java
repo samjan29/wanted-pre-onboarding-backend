38ded78834b45ce0c5391e1476fb6b759d1307fb
jan <samjan1127@gmail.com>
2024-08-13 19:30:49 +0900

    Refactor: 중복되는 경로 클래스 레벨로 이동

--- src/main/java/com/job/domain/controller/JobController.java
+++ src/main/java/com/job/domain/controller/JobController.java
@@ -7,24 +7,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/job")
 @RequiredArgsConstructor
 public class JobController {
 
     private final JobService jobService;
 
-    @PostMapping("/job")
+    @PostMapping
     public ResponseEntity<Void> createRecruitmentNotice(@RequestBody HandleRecruitmentNoticeDto handleRecruitmentNoticeDto) {
         jobService.createRecruitmentNotice(handleRecruitmentNoticeDto);
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/job/{recruitmentNoticeId}")
+    @PutMapping("/{recruitmentNoticeId}")
     public ResponseEntity<HandleRecruitmentNoticeDto> modifyRecruitmentNotice(@PathVariable(name = "recruitmentNoticeId") Long recruitmentNoticeId, @RequestBody HandleRecruitmentNoticeDto handleRecruitmentNoticeDto) {
         return ResponseEntity.ok(jobService.modifyRecruitmentNotice(recruitmentNoticeId, handleRecruitmentNoticeDto));
     }
 
-    @DeleteMapping("/job/{recruitmentNoticeId}")
+    @DeleteMapping("/{recruitmentNoticeId}")
     public ResponseEntity<Void> deleteRecruitmentNotice(@PathVariable(name = "recruitmentNoticeId") Long recruitmentNoticeId) {
         jobService.deleteRecruitmentNotice(recruitmentNoticeId);
         return ResponseEntity.ok().build();
